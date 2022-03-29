b8f2e9c76c61c4f8d16c980658c9cec953d2de68
pingyi Luo <pingyiluo@gmail.com>
2022-03-30 00:24:37 +0800

    feat: http拦截

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,1 +1,40 @@
 import axios from 'axios';
+
+const instance = axios.create({
+  headers: {},
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return {
+      ...config,
+      // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
+      xsrfCookieName: 'csrfToken',
+
+      // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
+      xsrfHeaderName: 'X-XSRF-TOKEN',
+    };
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
