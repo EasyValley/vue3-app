fd36725872d237db324f455442a00b3e96f67c88
pingyi Luo <pingyiluo@gmail.com>
2022-03-29 23:28:19 +0800

    :tada:

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://example.com',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/api': '/',
+        },
+      },
+      '/api-wss': {
+        ws: true,
+        target: 'http://example.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
